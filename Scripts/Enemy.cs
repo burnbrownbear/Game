ae39ac151d21f1ae4f691741f319bfe090113ec3
burnbrownbear <33675695+burnbrownbear@users.noreply.github.com>
2019-06-06 16:55:02 +0900

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public NavMeshAgent Agent;
+    public Text scoree;
+    int score = 0;
+
+    void Start()
+    {
+         Agent = GetComponent<NavMeshAgent>();
+         score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Agent.SetDestination(player.transform.position);
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Bullet")
+        {
+           Destroy(gameObject);
+           scoree.text = "Score : 0";
+           score = score + 1;
+           scoree.text = "Score : " + score;
+
+           
+           
+
+        }
+        if(score >= 1)
+            {
+            SceneManager.LoadScene(2); 
+            score = 0;
+            }
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text heal;
+    Rigidbody rb;
+    public GameObject bullet; 
+    GameObject cloneBullet;
+    Rigidbody cloneB; 
+    int health = 100;
+    int score;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        health = 100;
+        score = 0;
+        print("Health: " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	rb.AddForce(transform.forward * moveVertical * 50f);
+    	transform.Rotate(0f, moveHorizontal * 5f, 0);
+
+         if(Input.GetKeyDown("space"))
+       {
+          cloneBullet = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z+1f), Quaternion.identity);
+          cloneB = cloneBullet.GetComponent<Rigidbody>();
+          cloneB.AddForce(transform.forward * 1000f);
+
+       }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            health -= 1;
+            //Debug.Log("Health: " + health);
+            heal.text = "Health: " + health;
+
+        }
+          if(health <= 0)
+            {
+                SceneManager.LoadScene(3);
+                health = 100;
+            }
+    }
+}
